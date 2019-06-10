cd539fd4a4a3ee66549ef0a6435c29ef20ba2740
Josue <josue.ardon113@gmail.com>
2019-06-09 22:18:22 -0600

    Retrieving data using LiveData(all logic process is in view model class)

--- app/src/main/java/com/github/mvvmprojectexample/View/MovieFragmentView.kt
+++ app/src/main/java/com/github/mvvmprojectexample/View/MovieFragmentView.kt
@@ -1,14 +1,20 @@
 package com.github.mvvmprojectexample.View
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.github.mvvmprojectexample.R
+import com.github.mvvmprojectexample.Utils.DynamicAdapter
 import com.github.mvvmprojectexample.ViewModel.MovieViewModel
+import kotlinx.android.synthetic.main.item_movie.view.*
 import kotlinx.android.synthetic.main.view_fragment_movie.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -23,13 +29,19 @@ class MovieFragmentView: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //RecyclerView layout manager setup
+        moviesRecyclerView.layoutManager = GridLayoutManager(context, 1)
+
         //Creating ViewModel
         val viewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
 
         //On click function for Insert Button
         insertButton.setOnClickListener {
             insertTitle(viewModel)
+            retrieveMovies(viewModel)
         }
+
+        retrieveMovies(viewModel)
 
     }
 
@@ -38,17 +50,32 @@ class MovieFragmentView: Fragment() {
         //My edit text
         val input = insertMovieTitle.text.toString().trim()
 
+        //Validation edit text
         if (input.isEmpty()) {
             insertMovieTitle.setError("Title is required", resources.getDrawable(R.drawable.ic_error_outline_black_24dp))
             return
         }
 
+        //Save the movie
         GlobalScope.launch {
             viewModel.storeMovie(input)
         }
-        
+
         Toast.makeText(activity, "$input entered.", Toast.LENGTH_SHORT).show()
 
+    }
+
+    private fun retrieveMovies(viewModel: MovieViewModel) {
+        //Fetch the movies and observe it
+        viewModel.retrieveMovies().observe(this, Observer {
+            if (it.isNotEmpty()) {
+                moviesRecyclerView.adapter = DynamicAdapter(R.layout.item_movie, it, fun (_, view, movie, _) {
+                    view.movieTitle.text = movie.name
+                })
+            } else {
+                Log.d("debug", "No data available.")
+            }
+        })
     }
 
 }
--- app/src/main/java/com/github/mvvmprojectexample/ViewModel/MovieViewModel.kt
+++ app/src/main/java/com/github/mvvmprojectexample/ViewModel/MovieViewModel.kt
@@ -1,20 +1,40 @@
 package com.github.mvvmprojectexample.ViewModel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
 import com.github.mvvmprojectexample.Model.Movie
+import com.github.mvvmprojectexample.R
 import com.github.mvvmprojectexample.Room.MyDataBase
+import com.github.mvvmprojectexample.Utils.DynamicAdapter
+import kotlinx.android.synthetic.main.item_movie.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MovieViewModel(application: Application): AndroidViewModel(application) {
 
     //Get db instance
     private val movieDB: MyDataBase? = MyDataBase.getInstance(application)
+    //Live data
+    private val allMovies = MutableLiveData<List<Movie>>()
 
     //Store data
     fun storeMovie(title: String) {
         val movie = Movie()
         movie.name = title
         movieDB?.movieDao()?.insert(movie)
+    }
+
+    //Fetch movies from data base using Live Data
+    fun retrieveMovies(): LiveData<List<Movie>> {
+        GlobalScope.launch {
+            val list = movieDB?.movieDao()?.getAll() //Save list of movies in this var
+            allMovies.postValue(list)
+        }
+        return allMovies //Return movies
     }
 
 }
